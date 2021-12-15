ceca2664bf88505511f91b4cc135524a11213065
B0tta <luca.bottani00@gmail.com>
2021-12-15 02:10:11 +0100

    end2

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ let express = require("express");
 
 let app = express();
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 let server = app.listen(port);
 
-console.log("server is running on http://localhost:" + port);
+//console.log("server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
 
@@ -17,7 +17,7 @@ let io = serverSocket(server);
 io.on("connection", newConnection);
 
 function newConnection(newSocket) {
-  console.log(newSocket.id);
+  //console.log(newSocket.id);
 
   newSocket.on("mouse", mouseMessage);
 
