5af0058ddbfd3ba6853e65546e11571e269be56d
B0tta <luca.bottani00@gmail.com>
2021-12-15 01:20:25 +0100

    up

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ let port = 3000;
 
 let server = app.listen(port);
 
-console.log("servers is running on http://localhost:" + port);
+console.log("server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
